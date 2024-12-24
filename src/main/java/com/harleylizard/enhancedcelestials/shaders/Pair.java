9ef593618cf4b5370aff60549615a305df9d4f98
harleylizard <64570529+harleylizard@users.noreply.github.com>
2024-12-24 20:03:22 +1000

    wip virtual glsl file

--- src/main/java/com/harleylizard/enhancedcelestials/shaders/Pair.java
+++ src/main/java/com/harleylizard/enhancedcelestials/shaders/Pair.java
@@ -5,9 +5,9 @@ import org.joml.Vector3f;
 
 public record Pair(ColorSettings left, ColorSettings right) {
 
-    // Corgi Taco has daytime hard-coded to be neon blue. Replaced with white until solution is done.
+    // Corgi Taco has day and night hard-coded to be neon blue. Replaced with white until solution is done.
     public Vector3f mix(float t) {
-        return mix(ProgramExtension.WHITE, left.getGLSkyLightColor(), t);
+        return mix(ProgramExtension.WHITE, left.getSkyLightColor() == 0x3333FF ? ProgramExtension.WHITE : left.getGLSkyLightColor(), t);
     }
 
     private static Vector3f mix(Vector3f first, Vector3f second, float t) {
@@ -19,6 +19,6 @@ public record Pair(ColorSettings left, ColorSettings right) {
     }
 
     private static float parabola(float start, float end, float t) {
-        return (1.0F - t) * start + t * end - 4 * (t - 0.5F) * (t - 0.5F) * (end - start);
+        return (1.0F - t) * start + t * end - 4.0F * (t - 0.5F) * (t - 0.5F) * (end - start);
     }
 }
