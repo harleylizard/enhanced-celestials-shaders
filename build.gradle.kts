574c15cdfad6cda46160f7097019b1b8a2a46577
harleylizard <64570529+harleylizard@users.noreply.github.com>
2024-12-25 00:48:46 +1000

    21**

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,9 +37,9 @@ tasks.test {
 
 java {
     withSourcesJar()
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
